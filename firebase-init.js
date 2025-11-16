fcbaabb48d23e15675925559bf80bf2357ab01f7
Israel <osanisrael2@gmail.com>
2025-11-16 04:17:22 +0100

    Add files via upload

---
+++ firebase-init.js
@@ -0,0 +1,9 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js';
+import { getAuth } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js';
+import { getFirestore } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js';
+import { getStorage } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js';
+
+const app = initializeApp(CONFIG.firebase);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
